3c0fa9ae3a9b7d224454fc499370acfd557b146f
totsuan <nkkn1446count10@gmail.com>
2019-11-15 11:23:58 +0900

    fix

--- src/rtc/manager.cpp
+++ src/rtc/manager.cpp
@@ -10,6 +10,7 @@
 #include "modules/audio_processing/include/audio_processing.h"
 #include "modules/video_capture/video_capture_factory.h"
 #include "media/engine/webrtcvideocapturerfactory.h"
+#include "customfakevideocapturer.h"
 #include "rtc_base/ssladapter.h"
 #include "rtc_base/logging.h"
 
@@ -74,8 +75,9 @@ RTCManager::RTCManager(ConnectionSettings conn_settings, std::unique_ptr<cricket
 #else
 
     // capturer = createVideoCapturer();
-    _capturer = new CustomVideoCapturer();
-    std::unique_ptr<cricket::VideoCapturer> capturer(_capturer);
+    // _capturer = new CustomVideoCapturer();
+    auto c = new cricket::CustomFakeVideoCapturer(true);
+    std::unique_ptr<cricket::VideoCapturer> capturer(c);
 
     webrtc::FakeConstraints constraints;
         constraints.AddMandatory(webrtc::MediaConstraintsInterface::kMaxWidth, _conn_settings.getWidth());
